6e8433d5450c51f6ed7aa2796cdb3245c6951264
JohnLaurenceMalupa <117649828+JohnLaurenceMalupa@users.noreply.github.com>
2022-11-07 22:44:02 +0800

    Add files via upload
    age calculator

--- script.js
+++ script.js
@@ -1,82 +1,42 @@
-const date = new Date();
+var calculate = document.getElementById('calculate'),
+  calculateAge = document.getElementById('calculateAge');
 
-const renderCalendar = () => {
-  date.setDate(1);
+  calculate.addEventListener('click',function(){
+    var birthDate = document.getElementById('birth_date').value;
+    birthDate = new Date(birthDate);
+    todayDate = new Date()
+    totalAge = Date.now() - birthDate.getTime();
+    ageYear = new Date(totalAge);
+    ageYear = Math.abs(ageYear.getUTCFullYear() - 1970 );
+    
+    function ageMonth() {
+      if(todayDate.getMonth() >= birthDate.getMonth()) {
+          if(todayDate.getDate() >= birthDate.getDate()) {
+              return todayDate.getMonth() - birthDate.getMonth();
+          }
+          else {
+              if((todayDate.getMonth() - 1) >= birthDate.getMonth()) {
+                  return (todayDate.getMonth() - 1) - birthDate.getMonth();
+              }
+              else {
+                  return ((todayDate.getMonth() - 1) + 12) - birthDate.getMonth();
+              }
+          }
+      }
+        
+    };
+    function ageDay() {
+        if(todayDate.getDate() > birthDate.getDate()) {
+            return todayDate.getDate() - birthDate.getDate();
+        }
+        else if(todayDate.getDate() == birthDate.getDate()) {
+            return todayDate.getDate() - birthDate.getDate();
+        }
+        else {
+            let calDate = new Date(birthDate.getFullYear(), birthDate.getMonth(), 0);
+            return (todayDate.getDate() + calDate.getDate()) - birthDate.getDate();
+        }
+    };
+    calculateAge.innerHTML = "Your Age "+ageYear+" Years "+ageMonth()+ " Months "+ageDay()+" Days"
 
-  const monthDays = document.querySelector(".days");
-
-  const lastDay = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDate();
-
-  const prevLastDay = new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    0
-  ).getDate();
-
-  const firstDayIndex = date.getDay();
-
-  const lastDayIndex = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDay();
-
-  const nextDays = 7 - lastDayIndex - 1;
-
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-
-  document.querySelector(".date p").innerHTML = new Date().toDateString();
-
-  let days = "";
-
-  for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
-  }
-
-  for (let i = 1; i <= lastDay; i++) {
-    if (
-      i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<div class="today">${i}</div>`;
-    } else {
-      days += `<div>${i}</div>`;
-    }
-  }
-
-  for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
-    monthDays.innerHTML = days;
-  }
-};
-
-document.querySelector(".prev").addEventListener("click", () => {
-  date.setMonth(date.getMonth() - 1);
-  renderCalendar();
-});
-
-document.querySelector(".next").addEventListener("click", () => {
-  date.setMonth(date.getMonth() + 1);
-  renderCalendar();
-});
-
-renderCalendar();
+  });
